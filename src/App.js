97827240abc4b5f196306c952ae89050e19804f4
Samantha Bretous <sam@samanthabretous.com>
2016-10-02 19:00:34 -0400

    found winner for bottom-left to top-right

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-require("./App.css");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./App.css";
 
 import Game from './Game'
+const radix = 16;
 
-
-var App = React.createClass({
+const App = React.createClass({
   getInitialState() {
     return {
-      matrix:[["a",0,0,0,0,0],
+      matrix:[[0,0,0,0,0,0],
               [0,0,0,0,0,0],
-              ["c",0,0,0,0,0],
-              ["d",0,0,0,0,0],
-              ["e",0,0,0,0,0],
-              ["f",0,0,0,0,0],
-              ["g",0,0,0,0,0]],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0],
+              [0,0,0,0,0,0]],
       player: "r", 
       amountOfClicks: 0,
     };
   },
   handleClick(event) {
-    console.log(event.target.attributes.value.nodeValue)
+
     //find the column that the user clicks on 
-    var node = event.target.attributes.value.nodeValue;
+    let node = event.target.attributes.value.nodeValue;
     if(node){
 
       //iterate backwards through the column tp find available position
-      var newMatrix = this.state.matrix;
-      var arr = this.state.matrix[node]
-      for(var i = arr.length -1; i >= 0; i--){
+      let newMatrix = this.state.matrix;
+      let arr = this.state.matrix[node]
+      let i = null
+      for(i = arr.length -1; i >= 0; i--){
         if(arr[i] === 0){
           arr[i] = this.state.player;
           newMatrix[node] = arr
@@ -41,44 +42,26 @@ var App = React.createClass({
         }
       }
       
+   
+
+      //switch players
+      let player = this.state.player === "r" ? "b" : "r"
+      this.setState({matrix: newMatrix, player: player, amountOfClicks: ++this.state.amountOfClicks})
+
       //check to see if there is a winner when there can be the first possible win
       //if(amountofclicks < 7) {
         this.winners(newMatrix, node, i, this.state.player)
       //}
-
-      //switch players
-      var player = this.state.player === "r" ? "b" : "r"
-      this.setState({matrix: newMatrix, player: player, amountOfClicks: ++this.state.amountOfClicks})
-
       
     }
   },
-  winners(newMatrix, node, clickedIndex, player){
-    
-    // //check column wins-----------------------
-    // //check column wins-----------------------
-    if(clickedIndex < 3){
-      var counter = 1;
-      for(var i = clickedIndex; i < newMatrix[node].length; i++){
-        if(!(newMatrix[node][i] === newMatrix[node][i +1])){
-          break;
-        }
-        counter ++
-        if(counter === 4){
-          console.log("winner"); 
-        }
-      }
-    }
-
-    //check row wins-----------------------------
-    //check row wins-----------------------------
-    counter = 1;
-    i = parseInt(node);
-    // i=7; 
-    var k = parseInt(node);
+  horizontalWinner(newMatrix, node, clickedIndex, player){
+    let counter = 1;
+    let i = parseInt(node, radix);
+    let k = parseInt(node, radix);
     while(k > -1 || i < 7 ){
 
-      //checks for k
+      //checks to the left of the clickedIndex
       if(k > -1){
         if(k===0){
           k=-1;
@@ -88,15 +71,9 @@ var App = React.createClass({
         } else {
           k = -1
         }
-
-        if(counter === 4){
-          console.log("winner"); 
-          i=7;
-          k=-1;
-        } 
       }
 
-      //checks for i
+      //checks to the right of the clickedIndex
       if(i < 7){ 
         if(i === 6){
           i=7;
@@ -105,47 +82,90 @@ var App = React.createClass({
           i++;
         } else {
           i = 7;
-        }
-
-        if(counter === 4){
-          console.log("winner"); 
-          i=7;
-          k=-1;
         } 
       }
+
+      if(counter === 4){
+        console.log("winner"); 
+        i=7;
+        k=-1;
+      }
     }
+  },
+  verticalWinner(newMatrix, node, clickedIndex, player){
+    if(clickedIndex < 3){
+      let counter = 1;
+      for(let i = clickedIndex; i < newMatrix[node].length; i++){
+        if(!(newMatrix[node][i] === newMatrix[node][i +1])){
+          break;
+        }
+        counter ++
+        if(counter === 4){
+          console.log("winner"); 
+        }
+      }
+    }
+  },
+  bottomLeft_TopRight(newMatrix, node, clickedIndex, player){
 
-    // // //checks for dias from left to right
-    // counter = 1;
-    // i = parseInt(node);
-    // // i=7; 
-    // k = parseInt(node);
-    // while(k > -1 || i < 7 ){
-    //   console.log("counter", counter, "i", i, "k", k);
+    //checks for dias in the left to right direction
+    //----------------------------------------------
+    let counter = 1;
+    let i = parseInt(node, radix);
+    let k = parseInt(clickedIndex, radix);
 
-    //   if(newMatrix[i][k] === newMatrix[i+1][k-1]){
-    //     counter++;
-    //     i++
-    //     k--;
-    //   } else {
-    //     k = -1
-    //     i==7
-    //   }
+    let ii = parseInt(node, radix)
+    let kk = parseInt(clickedIndex, radix)
 
-    //     if(counter === 4){
-    //       console.log("winner"); 
-    //       i=7;
-    //       k=-1;
-    //     } 
-    //   }
+    while(k > -1 || i < 7 || ii > -1 || kk < 6){      
+      console.log(newMatrix[i][k], newMatrix[ii][kk])
 
-    //   i++
+      if(k > -1 || i < 7 ){
+        if(i === 6 || k === 0){
+          break;
+        }
+        if(newMatrix[i+1][k-1] !== undefined){
+          if(newMatrix[i][k] === newMatrix[i+1][k-1]){
+            console.log("enter")
+            counter++;
+            i++
+            k--;
+          } else {
+            k = -1
+            i = 7
+          }
+        }
+      }
+      if(ii > -1 || kk < 6){
+        if(ii === 0){
+          break;
+        }
 
-    //   if(i === 100 || k === -100){
-    //     break;
-    //   }
-    // }
+        if(newMatrix[ii][kk] === newMatrix[ii-1][kk+1]){
+          console.log("enter back")
+          counter++;
+          ii--;
+          kk++;
+        } else {
+          ii = -1
+          kk = 6
+        }
+        
+      }
 
+      if(counter === 4){
+        console.log("winner"); 
+        i=7;
+        k=-1;
+        ii= -1;
+        kk = 6
+      } 
+    }
+  },
+  winners(newMatrix, node, clickedIndex, player){
+    this.horizontalWinner(newMatrix, node, clickedIndex, player)
+    this.verticalWinner(newMatrix, node, clickedIndex, player)
+    this.bottomLeft_TopRight(newMatrix, node, clickedIndex, player)
   },
   render() {
     return (
